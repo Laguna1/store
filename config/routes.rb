f444e92c1131ea1942a6d234c4a492c85c868820
Laguna1 <lagunazoomarket@gmail.com>
2020-10-14 01:51:06 +0300

    add welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   devise_for :users
   scope '/admin' do
     resources :users
   end
+
   resources :items
   resources :roles
   resources :users
+
+  authenticated :user do
+    root to: 'items#index', as: :authenticated_root
+  end
   root to: 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
