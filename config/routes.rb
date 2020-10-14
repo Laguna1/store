40cae96aea3e0da779f522ce5ae32daa8906e19e
Laguna1 <lagunazoomarket@gmail.com>
2020-10-14 19:52:51 +0300

    fixed sign_out path

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
@@ -16,5 +17,4 @@ Rails.application.routes.draw do
     root to: 'items#index', as: :authenticated_root
   end
   root to: 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,10 @@ r1 = Role.create({ name: 'Regular', description: 'Can read items' })
 r2 = Role.create({ name: 'Seller', description: 'Can read and create items. Can update and destroy own items' })
 r3 = Role.create({ name: 'Admin', description: 'Can perform any CRUD operation on any resource' })
 
-u1 = User.create({ name: 'Sally', email: 'sally@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', role_id: r1.id })
+u1 = User.create({ name: 'Sally', email: 'sally@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', role_id: r3.id })
 u2 = User.create({ name: 'Sue', email: 'sue@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', role_id: r2.id })
 u3 = User.create({ name: 'Kev', email: 'kev@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', role_id: r2.id })
-u4 = User.create({ name: 'Jack', email: 'jack@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', role_id: r3.id })
+u4 = User.create({ name: 'Jack', email: 'jack@example.com', password: 'aaaaaaaa', password_confirmation: 'aaaaaaaa', role_id: r1.id })
 
 i1 = Item.create({ name: 'Rayban Sunglasses', description: 'Stylish shades', price: 99.99, user_id: u2.id })
 i2 = Item.create({ name: 'Gucci watch', description: 'Expensive timepiece', price: 199.99, user_id: u2.id })
